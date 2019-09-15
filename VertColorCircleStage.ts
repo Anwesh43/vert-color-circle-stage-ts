3280b23425b4ab096867d2245b248facf7bccb25
Anwesh43 <anweshthecool0@gmail.com>
2019-09-16 02:43:44 +0530

    adding Renderer in Stage

--- VertColorCircleStage.ts
+++ VertColorCircleStage.ts
@@ -50,6 +50,7 @@ class VertColorCircleStage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w
@@ -61,11 +62,14 @@ class VertColorCircleStage {
     render() {
         this.context.fillStyle = backColor
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
